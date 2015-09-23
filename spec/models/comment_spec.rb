179b1c76daedfa79acb6573d39c3d1d778a39a2c
Kirill Cherkasov <sky7hunder@gmail.com>
2015-09-23 22:20:10 +0300

    Adding twitter bootstrap. Adding stylesheets for website.

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+require 'comment'
+
+describe Comment do
+  it 'is created correct' do
+
+  end
+end
--- spec/models/offer_spec.rb
+++ spec/models/offer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'offer'
 
 describe Offer do
-  it "is created correct" do
+  it 'create' do
     offer = Offer.new(author: 'Ash', title: 'Football challenge', description: 'dfjgjflk ldfkjgklfd lkjdfg fg')
     offer.author.should == 'Ash'
   end
